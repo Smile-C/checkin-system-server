65baf1171bf69198e1efd96f2ed1c2654b63f3e4
Smile_C <Smile1767623462@163.com>
2026-04-11 11:50:28 +0800

    [Feature] 初始化后端项目
    Signed-off-by: Smile_C <Smile1767623462@163.com>

---
+++ src/test/java/com/smile/checkinsystem/CheckinSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.checkinsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
